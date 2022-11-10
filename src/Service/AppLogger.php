1d1415af68d14de31088e4cb3d0360f3485e97ab
JohnGoncy <ciray@163.com>
2022-11-11 02:14:28 +0800

    php test 2022-11-11

--- src/Service/AppLogger.php
+++ src/Service/AppLogger.php
@@ -2,31 +2,61 @@
 
 namespace App\Service;
 
+use think\facade\Log;
+
 class AppLogger
 {
     const TYPE_LOG4PHP = 'log4php';
+    const TYPE_THINKLOG = 'think-log';
 
     private $logger;
+    private $logType;
 
     public function __construct($type = self::TYPE_LOG4PHP)
     {
+        $this->logType = $type;//初始化日志引擎的类型（用到代理模式）
         if ($type == self::TYPE_LOG4PHP) {
             $this->logger = \Logger::getLogger("Log");
+        } elseif (self::TYPE_THINKLOG == $type) {
+            Log::init([
+                'default'	=>	'file',
+                'channels'	=>	[
+                    'file'	=>	[
+                        'type'	=>	'file',
+                        'path'	=>	'./logs/',
+                    ],
+                ],
+            ]);
         }
     }
 
     public function info($message = '')
     {
-        $this->logger->info($message);
+        if (self::TYPE_LOG4PHP == $this->logType) {
+            $this->logger->info($message);
+        } elseif (self::TYPE_THINKLOG == $this->logType) {
+            $message = strtoupper($message);//日志内容改为大写
+            Log::info($message);
+        }
     }
 
     public function debug($message = '')
     {
-        $this->logger->debug($message);
+        if (self::TYPE_LOG4PHP == $this->logType) {
+            $this->logger->debug($message);
+        } elseif (self::TYPE_THINKLOG == $this->logType) {
+            $message = strtoupper($message);//日志内容改为大写
+            Log::debug($message);
+        }
     }
 
     public function error($message = '')
     {
-        $this->logger->error($message);
+        if (self::TYPE_LOG4PHP == $this->logType) {
+            $this->logger->error($message);
+        } elseif (self::TYPE_THINKLOG == $this->logType) {
+            $message = strtoupper($message);//日志内容改为大写
+            Log::error($message);
+        }
     }
 }
--- src/Service/ProductHandler.php
+++ src/Service/ProductHandler.php
@@ -5,4 +5,90 @@ namespace App\Service;
 class ProductHandler
 {
 
+    /**
+     * 计算商品总金额
+     * @param $products
+     * @return int|mixed
+     */
+    public function getTotalPrice($products)
+    {
+        $totalPrice = 0;
+        foreach ($products as $product) {
+            $price = $product['price'] ?: 0;
+            $totalPrice += $price;
+        }
+
+        return $totalPrice;
+    }
+
+    /**
+     * 对数组的price进行降序排列
+     * @param $a
+     * @param $b
+     * @return int
+     */
+    private function rsortPrice($a, $b)
+    {
+        if($a['price'] == $b['price']) {
+            return 0;
+        }
+        return ($a['price'] > $b['price']) ? -1 : 1;
+    }
+
+    /**
+     * 根据种类筛选产品
+     * @param $products
+     * @param $filter
+     * @return array
+     */
+    private function getProductsByType($products, $filter)
+    {
+        //定义过滤后的商品
+        $filteredProducts = array();
+        //根据种类type过滤商品
+        foreach ($products as $product) {
+            if ($product['type'] == $filter) {
+                $filteredProducts[] = $product;
+            }
+        }
+        return $filteredProducts;
+    }
+
+
+    /**
+     * 按照商品的某个key进行筛选，并按照金额降序排列，返回排序后的数组
+     * @param $products
+     * @param $filter
+     * @return array
+     */
+    public function rsortPriceWithTypeFilter($products, $filter)
+    {
+        //根据种类筛选返回筛选后的商品
+        $filteredProducts = $this->getProductsByType($products, $filter);
+
+        //对商品按金额降序排列
+        uasort($filteredProducts, [$this, "rsortPrice"]);
+
+        return $filteredProducts;
+    }
+
+    /** 将商品中的创建日期转换为unix_timestamp
+     * @param $products
+     * @return array
+     */
+    public function transCreateAtToTimestamp($products){
+        return array_map([$this, "transDatetimeToUnixTimestamp"], $products);
+    }
+
+
+    /**
+     * 将数组中的create_at字段转换成unix_timestamp
+     * @param $arg
+     * @return mixed
+     */
+    private function transDatetimeToUnixTimestamp(&$arg)
+    {
+        $arg['create_at'] = strtotime($arg['create_at']);
+        return $arg;
+    }
 }
