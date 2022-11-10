74fd099e0db6d56deb5e4042c9fc46d208fd6727
JohnGoncy <ciray@163.com>
2022-11-11 02:47:52 +0800

    php test 2022-11-11 test/service

--- tests/Service/ProductHandlerTest.php
+++ tests/Service/ProductHandlerTest.php
@@ -63,6 +63,46 @@ class ProductHandlerTest extends TestCase
             $totalPrice += $price;
         }
 
-        $this->assertEquals(143, $totalPrice);
+        $productHandler = new ProductHandler();
+        $this->assertEquals($productHandler->getTotalPrice($this->products), $totalPrice);
+        //$this->assertEquals(143, $totalPrice);
+    }
+
+    public function testRsortPriceWithTypeFilter()
+    {
+        $productHandler = new ProductHandler();
+        $sortedProducts = [
+            [
+                'id' => 4,
+                'name' => 'Cup cake',
+                'type' => 'Dessert',
+                'price' => 35,
+                'create_at' => '2021-04-18 08:45:00',
+            ],
+            [
+                'id' => 5,
+                'name' => 'New York Cheese Cake',
+                'type' => 'Dessert',
+                'price' => 40,
+                'create_at' => '2021-04-19 14:38:00',
+            ],
+
+        ];
+        //考虑到排序结果受数组下标的影响，在测试用例中，对数组下标做倒序排列以达到修正的效果
+        krsort($sortedProducts);
+
+        $this->assertEquals($sortedProducts, $productHandler->rsortPriceWithTypeFilter($this->products, 'Dessert'));
+    }
+
+    public function testTransCreateAtToTimestamp()
+    {
+        //初始化期望商品数组
+        $expectedProducts = $this->products;
+        //转换create
+        foreach ($expectedProducts as $idx => $product) {
+            $expectedProducts[$idx]['create_at'] = strtotime($product['create_at']);
+        }
+        $productHandler = new ProductHandler();
+        $this->assertEquals($expectedProducts, $productHandler->transCreateAtToTimestamp($this->products));
     }
 }
